85318cd052d10f4477b730a87f180d37d80091f5
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-26 14:11:55 +0800

    [FizzBuzz][Victor] test: add test order number multiple of 3 and 7

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -91,4 +91,20 @@ public class FizzBuzzTest {
         assertEquals("FizzBuzz", actual);
     }
 
+    @Test
+    public void should_return_Buzz_when_count_off_given_order_number_multiple_of_3_and_7() {
+        //given
+
+        int order = 3 * 7;
+
+        FizzBuzz fizzbuzz = new FizzBuzz();
+
+        //when
+
+        String actual = fizzbuzz.countOff(order);
+
+        //then
+
+        assertEquals("FizzWhizz", actual);
+    }
 }
