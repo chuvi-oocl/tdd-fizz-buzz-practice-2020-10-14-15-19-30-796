b7eb4f95aec2417bbc4409d37975216438d14d2e
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-26 14:03:36 +0800

    [FizzBuzz][Victor] feat: complete test order number multiple of 3 and 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,15 +1,20 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    String result = "";
+
     public String countOff(int order) {
         if (order % 3 == 0) {
-            return "Fizz";
+            this.result += "Fizz";
         }
         if (order % 5 == 0) {
-            return "Buzz";
+            this.result += "Buzz";
         }
         if (order % 7 == 0) {
             return "Whizz";
+        }
+        if (!this.result.equals("")) {
+            return this.result;
         }
         return String.valueOf(order);
     }
