d791d09236c26e010e6ccbeb752d6a702e717989
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-26 13:52:32 +0800

    [FizzBuzz][Victor] test: add test order number 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,24 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_1_when_count_off_given_order_number_1() {
+        //given
+
+        int order = 1;
+
+        FizzBuzz fizzbuzz = new FizzBuzz();
+
+        //when
+
+        String actual = fizzbuzz.countOff(order);
+
+        //then
+
+        assertEquals("1", actual);
+    }
 }
